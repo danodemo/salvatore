a9dfc8373213e93b9d13154495f2101766e7a80f
Dan Nicodemo <dan@showcaseidx.com>
2018-04-04 10:43:24 -0400

    More framework stuff

--- main.rb
+++ main.rb
@@ -1,11 +1,26 @@
+# require './listing_values.rb'
+# require './listing_template.rb'
+
+def choice
+	puts "Press 'Y' to continue, or anything else to quit!"
+	choice = gets.chomp
+	return if choice.chomp == 'Y'
+end
 
 def prompt
-	puts "Ok let's go"
+	puts "Welcome to the Fake Listing generator!"
+	choice
+end
+
+def spawn_doc
+	LISTING = {}
+	puts "Listing created.  Press 'Y' to continue, or anything else to quit"
+	choice
 end
 
 def main
 	prompt
-	# spawn_doc
+	spawn_doc
 	# build_listing
 	# save
 end
