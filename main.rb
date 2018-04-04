d54e0789f5dbd7cf88c514737d946eb145b3a5b9
Dan Nicodemo <dan@showcaseidx.com>
2018-04-04 11:01:13 -0400

    Add some variables and define our fields

--- main.rb
+++ main.rb
@@ -1,21 +1,72 @@
 # require './listing_values.rb'
 # require './listing_template.rb'
+listing = nil
+fields = [
+        :internal_id,
+        :orig_mls_number,
+        :mls_number,
+        :price_change_dts,
+        :img_change_dts,
+        :first_seen_dts,
+        :sale_type,
+        :listing_type,
+        :property_type,
+        :property_type_id,
+        :status,
+        :foreclosure,
+        :short_sale,
+        :description,
+        :virtual_tour_url,
+        :listing_office,
+        :listing_office_id,
+        :listing_agent,
+        :listing_agent_id,
+        :colisting_agent_id,
+        :bedrooms,
+        :bathrooms,
+        :halfbaths,
+        :price,
+        :taxes,
+        :tax_year,
+        :hoa_monthly,
+        :year_built,
+        :sqft,
+        :sqft_source,
+        :acres,
+        :school_district,
+        :elementary_school,
+        :middle_school,
+        :high_school,
+        :mls_area,
+        :county,
+        :city,
+        :state,
+        :citystate,
+        :zip,
+        :address,
+        :subdivision,
+        :street_name,
+        :building,
+        :unit_number,
+        :latitude,
+        :longitude
+    ]
 
-def choice
+def continue_yn
 	puts "Press 'Y' to continue, or anything else to quit!"
 	choice = gets.chomp
-	return if choice.chomp == 'Y'
+	choice.chomp == 'Y' ? return : exit
 end
 
 def prompt
 	puts "Welcome to the Fake Listing generator!"
-	choice
+	continue_yn
 end
 
 def spawn_doc
-	LISTING = {}
+	listing = {}
 	puts "Listing created.  Press 'Y' to continue, or anything else to quit"
-	choice
+	continue_yn
 end
 
 def main
@@ -26,3 +77,13 @@ def main
 end
 
 main
+
+
+
+# program start
+# prompt
+# prepare a document
+# start filling fields
+# save listing
+# Prompt to make a new listing
+# push all listings into elasticsearch
