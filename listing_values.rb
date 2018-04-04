b8bde26046580d854cb170cf60b9d976cb0e10a4
Dan Nicodemo <dan@showcaseidx.com>
2018-04-04 10:36:02 -0400

    Initial commit and some basic framework stuff

---
+++ listing_values.rb
@@ -0,0 +1,5 @@
+
+PROP_REMARKS = [
+	"This beautiful, recently rennovated home is ready for the right family to move in immediately!  Elegant edges, smooth corners, and plenty of room to expand or modify the existing structure.  Take in the area's assorted sites and sounds and enjoy the life you've always dreamed of!  Buyer's are very motivated to sell, so move fast: this won't last long!  Part of the local Housing Association.",
+	"Nestled in one of the hottest areas in town, this little gem is waiting for the right buyer to really shine!  Historic craftsmanship meets modern engineering in this updated property with room to expand.  You'll be captivated by the remarkable trim, the detailed exterior, and the "
+]
---
+++ main.rb
@@ -0,0 +1,13 @@
+
+def prompt
+	puts "Ok let's go"
+end
+
+def main
+	prompt
+	# spawn_doc
+	# build_listing
+	# save
+end
+
+main
